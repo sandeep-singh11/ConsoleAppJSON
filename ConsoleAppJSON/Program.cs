197ac523e8b4d4a9c668a7ef08fe1acd8900fadf
sandeep <sahibbandgi11@gmail.com>
2021-02-16 11:43:18 -0800

    Add project files.

---
+++ ConsoleAppJSON/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            StudentInfoClass sic = new StudentInfoClass()
+            {
+                Roll = 1,
+                name = "Sandeep",
+                courses = new List<string>()
+       {
+         "Maths",
+         "Science",
+         "Data Structure",
+         ".Net"
+        }
+            };
+
+            string stringjson = JsonConvert.SerializeObject(sic);
+            Console.WriteLine(stringjson);
+        }
+
+    }
+}
---
+++ ConsoleAppJSON/StudentInfoClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppJSON
+{
+    class StudentInfoClass
+    {
+        
+            public int Roll { get; set; }
+            public string name { get; set; }
+            public List<string> courses { get; set; }
+        
+    }
+}
